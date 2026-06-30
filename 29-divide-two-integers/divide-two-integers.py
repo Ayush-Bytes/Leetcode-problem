29cc68adfb374cb05e21200154aa336227132846
Ayush Kumar <kumarayush39138@gmail.com>
2026-07-01 00:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (91.86%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        x=int(dividend/divisor)
+
+        if x == 2147483648:
+            return 2147483647
+        return x
