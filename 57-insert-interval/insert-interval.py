d1c8bc305d3f4cadc6c1bb3392cfb5c5539eabe0
Ayush Kumar <kumarayush39138@gmail.com>
2026-08-29 23:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (57.09%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -8,7 +8,6 @@ class Solution:
             result.append(intervals[i])
             i += 1
             
-        # Step 2: Merge all overlapping intervals with newInterval
         while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
