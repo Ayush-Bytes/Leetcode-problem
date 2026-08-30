efe39d1b5ea266ae246a88a754d5492ee30c7ea5
Ayush Kumar <kumarayush39138@gmail.com>
2026-08-31 00:04:09 +0530

    Time: 20 ms (57.70%) | Memory: 33.4 MB (80.97%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -4,14 +4,11 @@ class Solution:
         if n == 1:
             return 1
         
-        # Find indices of min and max elements
         min_idx = nums.index(min(nums))
         max_idx = nums.index(max(nums))
         
-        # Ensure i is the smaller index and j is the larger index
         i, j = min(min_idx, max_idx), max(min_idx, max_idx)
         
-        # Scenario 1: Remove both from the front
         option1 = j + 1
         
         # Scenario 2: Remove both from the back
