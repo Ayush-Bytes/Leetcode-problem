ba0a55072d46f901d19c804887d287618ded9c07
Ayush Kumar <kumarayush39138@gmail.com>
2026-08-31 00:05:33 +0530

    Time: 1379 ms (56.04%) | Memory: 19.6 MB (65.22%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -29,13 +29,11 @@ class Solution:
             r, c = empty_cells[index]
             b = (r // 3) * 3 + (c // 3)
             
-            # Find digits 1-9 that are not used in row, col, or box
             used = rows[r] | cols[c] | boxes[b]
             
             for digit in range(1, 10):
                 mask = 1 << digit
                 if not (used & mask):
-                    # Place digit & update masks
                     board[r][c] = str(digit)
                     rows[r] |= mask
                     cols[c] |= mask
@@ -44,7 +42,6 @@ class Solution:
                     if backtrack(index + 1):
                         return True
                     
-                    # Backtrack (reset digit & masks)
                     board[r][c] = '.'
                     rows[r] ^= mask
                     cols[c] ^= mask
