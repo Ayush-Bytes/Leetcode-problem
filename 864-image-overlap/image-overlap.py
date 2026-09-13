da2c2e593097e299d6806aa332761359b02ae5e8
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-13 23:39:29 +0530

    Time: 190 ms (77.51%) | Memory: 19.9 MB (28.84%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -2,11 +2,9 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         n = len(img1)
         
-        # Collect coordinates of all 1s in both images
         list1 = [(r, c) for r in range(n) for c in range(n) if img1[r][c] == 1]
         list2 = [(r, c) for r in range(n) for c in range(n) if img2[r][c] == 1]
         
-        # Count the frequency of each translation vector
         translation_counts = collections.defaultdict(int)
         
         for r1, c1 in list1:
