96cfe2fa3698f44f55db403b725d9f0a3d8bc608
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-13 23:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (59.25%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -10,10 +10,8 @@ class Solution:
         if not root:
             return False
         
-        # Leaf node check: Agar dono children None hain
         if not root.left and not root.right:
             return root.val == targetSum
         
-        # Remaining sum ke liye subtrees check karo
         remaining_sum = targetSum - root.val
         return self.hasPathSum(root.left, remaining_sum) or self.hasPathSum(root.right, remaining_sum)
