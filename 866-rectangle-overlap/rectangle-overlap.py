65b8de2bed48092eb34439aca6211c120dac908b
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-14 22:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.18%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.py
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -4,11 +4,8 @@ class Solution:
         x1, y1, x2, y2 = rec1
         x3, y3, x4, y4 = rec2
         
-        # Check for non-overlapping conditions:
         # 1. rec1 is to the left of rec2
-        # 2. rec1 is to the right of rec2
         # 3. rec1 is below rec2
-        # 4. rec1 is above rec2
         is_not_overlapping = (x2 <= x3) or (x1 >= x4) or (y2 <= y3) or (y1 >= y4)
         
         return not is_not_overlapping
