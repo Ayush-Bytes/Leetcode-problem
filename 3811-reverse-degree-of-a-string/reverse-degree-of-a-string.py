1c55fedc809484ef095dcaf3329412f6e22b1acc
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-20 22:43:55 +0530

    Time: 11 ms (20.35%) | Memory: 19.3 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -3,7 +3,6 @@ class Solution:
   def reverseDegree(self, s: str) -> int:
     total_degree = 0
     for i, char in enumerate(s):
-      # 1-indexed position in the string
       string_index = i + 1
       # Position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1)
       reversed_alphabet_index = 26 - (ord(char) - ord('a'))
