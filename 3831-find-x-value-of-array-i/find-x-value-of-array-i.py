65beff48b6932338c6e11c1c4a2b46cd3caaa100
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-21 23:12:25 +0530

    Time: 358 ms (63.16%) | Memory: 34.2 MB (52.63%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -3,17 +3,13 @@ class Solution:
         n = len(nums)
         result = [0] * k
         
-        # dp[r] keeps track of the number of valid non-empty subarrays 
-        # ending at the previous index that yield a product modulo k equal to r.
         dp = [0] * k
         
         for num in nums:
             val = num % k
             new_dp = [0] * k
-            # A single-element subarray containing just the current number
             new_dp[val] += 1
             
-            # Extend all valid subarrays ending at the previous index
             for r in range(k):
                 if dp[r] > 0:
                     new_dp[(r * val) % k] += dp[r]
