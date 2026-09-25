d1d933ff58f1f8672ee571ba14a4fed5755ddaca
Ayush Kumar <kumarayush39138@gmail.com>
2026-09-25 23:54:27 +0530

    Time: 3 ms (79.78%) | Memory: 19.4 MB (81.97%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -5,15 +5,12 @@ class Solution:
 
         for char in expression:
             if char.isalpha():
-                # Concatenation: Append character to all current combinations
                 cur = {c + char for c in cur}
             elif char == '{':
-                # Save current state on stack
                 stack.append(res)
                 stack.append(cur)
                 res, cur = set(), set([""])
             elif char == ',':
-                # Union operation: Combine current with previous results
                 res.update(cur)
                 cur = set([""])
             elif char == '}':
@@ -22,7 +19,6 @@ class Solution:
                 prev_cur = stack.pop()
                 prev_res = stack.pop()
                 
-                # Multiply previous state with expanded inside scope
                 cur = {p + r for p in prev_cur for r in res}
                 res = prev_res
 
